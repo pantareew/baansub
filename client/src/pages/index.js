2dfb0780c893bec95a76af6187fbb19e46732a7b
pantareew <pantaree1459@gmail.com>
2023-12-18 19:10:08 +1100

    clear cache

--- client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home";
-export { default as History } from "./History";
-export { default as Rdp } from "./Rdp";
-export { default as Services } from "./Services";
-export { default as Events } from "./Events";
-export { default as Contact } from "./Contact";
-export { default as NoPage } from "./NoPage";
